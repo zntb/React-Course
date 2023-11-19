c4b637095c4fd98f52b56ece2300b9a816074ab9
zntb <zenetibi@yahoo.com>
2023-11-19 10:51:12 +0200

    Add the special {children} prop

--- 1-React Essentials/src/App.jsx
+++ 1-React Essentials/src/App.jsx
@@ -1,6 +1,7 @@
 import { CORE_CONCEPTS } from './data.js';
 import Header from './components/Header/Header.jsx';
 import CoreConcept from './components/CoreConcept.jsx';
+import TabButton from './components/TabButton.jsx';
 
 function App() {
   return (
@@ -19,6 +20,12 @@ function App() {
             <CoreConcept {...CORE_CONCEPTS[2]} />
             <CoreConcept {...CORE_CONCEPTS[3]} />
           </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton>Components</TabButton>
+          </menu>
         </section>
       </main>
     </div>
