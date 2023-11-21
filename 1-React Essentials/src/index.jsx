1dd60f7182fb8c4ac9a376002bf1755d6742e71e
zntb <zenetibi@yahoo.com>
2023-11-21 06:56:23 +0200

    Add Examples.jsx, CoreConcepts.jsx and splitting the components by features & State

--- 1-React Essentials/src/index.jsx
+++ 1-React Essentials/src/index.jsx
@@ -1,7 +1,9 @@
-import ReactDOM from "react-dom/client";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-import App from "./App.jsx";
-import "./index.css";
+import App from './App.jsx';
+import './index.css';
 
-const entryPoint = document.getElementById("root");
+const entryPoint = document.getElementById('root');
 ReactDOM.createRoot(entryPoint).render(<App />);
+// ReactDOM.createRoot(entryPoint).render(React.createElement(App));
