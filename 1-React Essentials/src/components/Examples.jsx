0859b6bf6584c7a1285178afd67a2558a3970caf
zntb <zenetibi@yahoo.com>
2023-11-22 06:10:29 +0200

    Setting component types dynamically

--- 1-React Essentials/src/components/Examples.jsx
+++ 1-React Essentials/src/components/Examples.jsx
@@ -29,6 +29,7 @@ export default function Examples() {
   return (
     <Section title="Examples" id="examples">
       <Tabs
+        ButtonsContainer="menu"
         buttons={
           <>
             <TabButton
