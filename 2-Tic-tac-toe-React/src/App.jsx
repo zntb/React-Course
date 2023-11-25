9cacf7dbc3f56faa6dc9d7e574a2739e8f84e9c5
zntb <zenetibi@yahoo.com>
2023-11-25 07:09:40 +0200

    Deriving Computed Values From Other Computed Values

--- 2-Tic-tac-toe-React/src/App.jsx
+++ 2-Tic-tac-toe-React/src/App.jsx
@@ -37,7 +37,23 @@ function App() {
     gameBoard[row][col] = player;
   }
 
+  let winner;
+
   for (const combination of WINNING_COMBINATIONS) {
+    const firstSquareSymbol =
+      gameBoard[combination[0].row][combination[0].column];
+    const secondSquareSymbol =
+      gameBoard[combination[1].row][combination[1].column];
+    const thirdSquareSymbol =
+      gameBoard[combination[2].row][combination[2].column];
+
+    if (
+      firstSquareSymbol &&
+      firstSquareSymbol === secondSquareSymbol &&
+      firstSquareSymbol === thirdSquareSymbol
+    ) {
+      winner = firstSquareSymbol;
+    }
   }
 
   function handleSelectSquare(rowIndex, colIndex) {
@@ -69,6 +85,7 @@ function App() {
             isActive={activePlayer === 'O'}
           />
         </ol>
+        {winner && <p>You won, {winner}!</p>}
         <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard} />
       </div>
       <Log turns={gameTurns} />
