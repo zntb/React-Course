c85471cbdb6293d1bdd906e01848b4f47dfb01d7
zntb <zenetibi@yahoo.com>
2023-11-26 07:49:53 +0200

    Add Handling Events & Using Two-Way-Binding

--- 3-Investment Calculator/src/App.jsx
+++ 3-Investment Calculator/src/App.jsx
@@ -1,10 +1,12 @@
 import Header from './components/Header.jsx';
+import UserInput from './components/UserInput.jsx';
 import './index.css';
 
 function App() {
   return (
     <>
       <Header />
+      <UserInput />
     </>
   );
 }
