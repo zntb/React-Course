82c000f6a2f6d0a08a4c165250a415aa0e72d795
zntb <zenetibi@yahoo.com>
2023-11-26 10:00:43 +0200

    Outputting Results in a List & Deriving More Values

--- 3-Investment Calculator/src/components/Results.jsx
+++ 3-Investment Calculator/src/components/Results.jsx
@@ -2,7 +2,40 @@ import { calculateInvestmentResults, formatter } from '../util/investment.js';
 
 export default function Results({ input }) {
   const reultsData = calculateInvestmentResults(input);
+  const initialInvestment =
+    reultsData[0].valueEndOfYear -
+    reultsData[0].interest -
+    reultsData[0].annualInvestment;
 
-  console.log(reultsData);
-  return <div></div>;
+  return (
+    <table id="result">
+      <thead>
+        <tr key="">
+          <th>Year</th>
+          <th>Investment Value</th>
+          <th>Interest (Year)</th>
+          <th>Total Interest</th>
+          <th>Invested capital</th>
+        </tr>
+      </thead>
+      <tbody>
+        {reultsData.map((yearData) => {
+          const totalInterest =
+            yearData.valueEndOfYear -
+            yearData.annualInvestment * yearData.year -
+            initialInvestment;
+          const totalAmountinvested = yearData.valueEndOfYear - totalInterest;
+          return (
+            <tr key={yearData.year}>
+              <td>{yearData.year}</td>
+              <td>{formatter.format(yearData.valueEndOfYear)}</td>
+              <td>{formatter.format(yearData.interest)}</td>
+              <td>{formatter.format(totalInterest)}</td>
+              <td>{formatter.format(totalAmountinvested)}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
 }
