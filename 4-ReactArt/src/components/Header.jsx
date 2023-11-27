cf7fc2cb069b79f85003e09e8ba9a352c52ec445
zntb <zenetibi@yahoo.com>
2023-11-27 06:51:11 +0200

    Styling React Apps with Inline Styles

--- 4-ReactArt/src/components/Header.jsx
+++ 4-ReactArt/src/components/Header.jsx
@@ -1,11 +1,15 @@
 import logo from '../assets/logo.png';
+import './Header.css';
 
 export default function Header() {
   return (
     <header>
       <img src={logo} alt="A canvas" />
       <h1>ReactArt</h1>
-      <p>A community of artists and art-lovers.</p>
+      //Inline styles
+      <p style={{ color: 'red', 'text-align': 'center' }}>
+        A community of artists and art-lovers.
+      </p>
     </header>
   );
 }
