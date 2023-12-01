97183fae1b12a280c1f8f2737d287be14e3b17ae
zntb <zenetibi@yahoo.com>
2023-12-01 09:00:08 +0200

    Add TimerChallenge component

--- 5-The-Almost-Final-Countdown/src/components/Player.jsx
+++ 5-The-Almost-Final-Countdown/src/components/Player.jsx
@@ -4,10 +4,11 @@ export default function Player() {
   const playerName = useRef();
 
   const [enteredPlayerName, setEnteredPlayerName] = useState(null);
-  const [submitted, setSubmitted] = useState(false);
+  // const [submitted, setSubmitted] = useState(false);
 
   function handleClick() {
     setEnteredPlayerName(playerName.current.value);
+    playerName.current.value = '';
   }
 
   return (
---
+++ 5-The-Almost-Final-Countdown/src/components/TimerChallenge.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react';
+
+export default function TimerChallenge({ title, targetTime }) {
+  return (
+    <section className="challenge">
+      <h2>{title}</h2>
+
+      <p className="challenge-time">
+        {targetTime} second{targetTime > 1 ? 's' : ''}
+      </p>
+      <p>
+        <button>Start Challenge</button>
+      </p>
+      <p className="">Time is running.../ Timer inactive</p>
+    </section>
+  );
+}
