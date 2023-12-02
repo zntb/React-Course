d00e6c14c7297a654b877e4a417a5d52daeb3de8
zntb <zenetibi@yahoo.com>
2023-12-02 08:03:58 +0200

    Closing the Modal via the ESC

--- 5-The-Almost-Final-Countdown/src/components/ResultModal.jsx
+++ 5-The-Almost-Final-Countdown/src/components/ResultModal.jsx
@@ -19,7 +19,7 @@ const ResultModal = forwardRef(function ResultModal(
   });
 
   return (
-    <dialog ref={dialog} className="result-modal">
+    <dialog ref={dialog} className="result-modal" onClose={onReset}>
       {userLost && <h2>You lost</h2>}
       {!userLost && <h2>Your score: {score}</h2>}
       <p>
