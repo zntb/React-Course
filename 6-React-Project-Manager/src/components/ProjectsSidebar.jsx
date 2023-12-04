fa6b1ee0cc59fd4a265c30ad6ef7e7eaf0d98749
zntb <zenetibi@yahoo.com>
2023-12-04 07:28:07 +0200

    Adding a Projects Sidebar Component

---
+++ 6-React-Project-Manager/src/components/ProjectsSidebar.jsx
@@ -0,0 +1,11 @@
+export default function ProjectsSidebar(props) {
+  return (
+    <aside>
+      <h2>Your projects</h2>
+      <div>
+        <button>+ Add Project</button>
+      </div>
+      <ul></ul>
+    </aside>
+  );
+}
