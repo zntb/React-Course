c5f66f9c6fb9f5417b3c89398bbecb031ac1d41b
zntb <zenetibi@yahoo.com>
2023-12-06 11:04:29 +0200

    Linking the Context to State

--- 7-Elegant-Context/src/App.jsx
+++ 7-Elegant-Context/src/App.jsx
@@ -67,8 +67,13 @@ function App() {
     });
   }
 
+  const ctxValue = {
+    items: shoppingCart.items,
+    addItemtoCart: handleAddItemToCart,
+  };
+
   return (
-    <CartContext.Provider value={{ items: [] }}>
+    <CartContext.Provider value={ctxValue}>
       <Header
         cart={shoppingCart}
         onUpdateCartItemQuantity={handleUpdateCartItemQuantity}
--- 7-Elegant-Context/src/components/Product.jsx
+++ 7-Elegant-Context/src/components/Product.jsx
@@ -1,22 +1,21 @@
-export default function Product({
-  id,
-  image,
-  title,
-  price,
-  description,
-  onAddToCart,
-}) {
+import { useContext } from 'react';
+
+import { CartContext } from '../store/shopping-cart-context';
+
+export default function Product({ id, image, title, price, description }) {
+  const { addItemtoCart } = useContext(CartContext);
+
   return (
     <article className="product">
       <img src={image} alt={title} />
       <div className="product-content">
         <div>
           <h3>{title}</h3>
-          <p className='product-price'>${price}</p>
+          <p className="product-price">${price}</p>
           <p>{description}</p>
         </div>
-        <p className='product-actions'>
-          <button onClick={() => onAddToCart(id)}>Add to Cart</button>
+        <p className="product-actions">
+          <button onClick={() => addItemtoCart(id)}>Add to Cart</button>
         </p>
       </div>
     </article>
