970c896f59aae1a5c0b0cb688d0bd03249172d67
zntb <zenetibi@yahoo.com>
2023-12-08 09:14:01 +0200

    Not All Side Effects Need useEffect

--- 8-Side-Effects-useEffect/src/App.jsx
+++ 8-Side-Effects-useEffect/src/App.jsx
@@ -42,6 +42,15 @@ function App() {
       const place = AVAILABLE_PLACES.find((place) => place.id === id);
       return [place, ...prevPickedPlaces];
     });
+
+    // this is a side effect, but no need useEffect!
+    const storedIds = JSON.parse(localStorage.getItem('selectedPlaces')) || [];
+    if (storedIds.indexOf(id) === -1) {
+      localStorage.setItem(
+        'selectedPlaces',
+        JSON.stringify([id, ...storedIds])
+      );
+    }
   }
 
   function handleRemovePlace() {
