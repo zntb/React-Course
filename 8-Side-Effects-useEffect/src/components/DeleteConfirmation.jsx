907b618997045b6e3ace6632551b5979ebf0ee65
zntb <zenetibi@yahoo.com>
2023-12-09 07:01:34 +0200

    Introducing useEffect's Cleanup Function

--- 8-Side-Effects-useEffect/src/components/DeleteConfirmation.jsx
+++ 8-Side-Effects-useEffect/src/components/DeleteConfirmation.jsx
@@ -1,8 +1,17 @@
+import { useEffect } from 'react';
+
 export default function DeleteConfirmation({ onConfirm, onCancel }) {
-  console.log('Timer set');
-  setTimeout(() => {
-    onConfirm();
-  }, 3000);
+  useEffect(() => {
+    console.log('Timer set');
+    const timer = setTimeout(() => {
+      onConfirm();
+    }, 3000);
+
+    return () => {
+      console.log('Cleaning up timer');
+      clearTimeout(timer);
+    };
+  }, []);
 
   return (
     <div id="delete-confirmation">
