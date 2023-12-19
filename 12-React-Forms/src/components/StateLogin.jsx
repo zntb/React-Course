e0b6835a88ee85aef25b01e32902d1c3ac137b4c
zntb <zenetibi@yahoo.com>
2023-12-19 10:02:44 +0200

    Validating Input Upon Lost Focus (Blur)

--- 12-React-Forms/src/components/StateLogin.jsx
+++ 12-React-Forms/src/components/StateLogin.jsx
@@ -6,8 +6,12 @@ export default function Login() {
     password: '',
   });
 
-  const emailIsInvalid =
-    enteredValues.email !== '' && !enteredValues.email.includes('@');
+  const [didEdit, setDidEdit] = useState({
+    email: false,
+    password: false,
+  });
+
+  const emailIsInvalid = didEdit.email && !enteredValues.email.includes('@');
 
   function handleSubmit(event) {
     event.preventDefault();
@@ -26,8 +30,18 @@ export default function Login() {
       // Allows acces dynamically a property in an object by adding square brackets [identifier].
       [identifier]: value,
     }));
+    setDidEdit((prevEdit) => ({
+      ...prevEdit,
+      [identifier]: false,
+    }));
   }
 
+  function handleInputBlur(identifier) {
+    setDidEdit((prevEdit) => ({
+      ...prevEdit,
+      [identifier]: true,
+    }));
+  }
   return (
     <form onSubmit={handleSubmit}>
       <h2>Login</h2>
@@ -39,6 +53,7 @@ export default function Login() {
             id="email"
             type="email"
             name="email"
+            onBlur={() => handleInputBlur('email')}
             onChange={(event) => handleInputChange('email', event.target.value)}
             value={enteredValues.email}
           />
