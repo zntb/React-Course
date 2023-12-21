ffed22e0eb3b062477bbe8d7ad37c9aa1c38ad6c
zntb <zenetibi@yahoo.com>
2023-12-21 17:07:16 +0200

    Add Cart Component

---
+++ 13-React FoodOrder/src/components/Cart.jsx
@@ -0,0 +1,32 @@
+import { useContext } from 'react';
+import Modal from './UI/Modal.jsx';
+import CartContext from '../store/CartContext.jsx';
+import { currencyFormatter } from '../util/formatting.js';
+import Button from './UI/Button.jsx';
+
+export default function Cart() {
+  const cartCtx = useContext(CartContext);
+
+  const cartTotal = cartCtx.items.reduce(
+    (totalPrice, item) => totalPrice + item.quantity * item.price,
+    0
+  );
+
+  return (
+    <Modal className="cart">
+      <h2>Your Cart</h2>
+      <ul>
+        {cartCtx.items.map((item) => (
+          <li key={item.id}>
+            {item.name} - {item.quantity}
+          </li>
+        ))}
+      </ul>
+      <p className="cart-total">{currencyFormatter.format(cartTotal)}</p>
+      <p className="modal-actions">
+        <Button textOnly>Close</Button>
+        <Button>Go to Checkout</Button>
+      </p>
+    </Modal>
+  );
+}
