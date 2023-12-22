09ca4e487cdcdfa35a8b930dc537b20d4d199027
zntb <zenetibi@yahoo.com>
2023-12-22 10:50:02 +0200

    Adding a Custom Input Component & Managing Modal Visibility

--- 13-React FoodOrder/src/components/Cart.jsx
+++ 13-React FoodOrder/src/components/Cart.jsx
@@ -19,8 +19,16 @@ export default function Cart() {
     useProgressCtx.hideCart();
   }
 
+  function handleGoToCheckout() {
+    useProgressCtx.showCheckout();
+  }
+
   return (
-    <Modal className="cart" open={useProgressCtx.progress === 'cart'}>
+    <Modal
+      className="cart"
+      open={useProgressCtx.progress === 'cart'}
+      onClose={useProgressCtx.progress === 'cart' ? handleCloseCart : null}
+    >
       <h2>Your Cart</h2>
       <ul>
         {cartCtx.items.map((item) => (
@@ -36,10 +44,12 @@ export default function Cart() {
       </ul>
       <p className="cart-total">{currencyFormatter.format(cartTotal)}</p>
       <p className="modal-actions">
-        <Button textOnly onClick={handleCloseCart}>
+        <Button textOnly className="close-button" onClick={handleCloseCart}>
           Close
         </Button>
-        <Button>Go to Checkout</Button>
+        {cartCtx.items.length > 0 && (
+          <Button onClick={handleGoToCheckout}>Go to Checkout</Button>
+        )}
       </p>
     </Modal>
   );
